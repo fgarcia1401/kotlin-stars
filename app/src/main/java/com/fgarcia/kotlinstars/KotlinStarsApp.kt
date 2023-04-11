390404baeb73561f7cc92292c919d248f73cd922
Fernando Garcia <tqi_fegarcia@uolinc.com>
2023-04-10 22:52:36 -0300

    Initial commit

---
+++ app/src/main/java/com/fgarcia/kotlinstars/KotlinStarsApp.kt
@@ -0,0 +1,5 @@
+package com.fgarcia.kotlinstars
+
+import android.app.Application
+
+class KotlinStarsApp : Application()
---
+++ app/src/main/java/com/fgarcia/kotlinstars/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fgarcia.kotlinstars.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fgarcia.kotlinstars.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
