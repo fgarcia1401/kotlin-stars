ac787b247be50b580e4f9fd4c101a7211227947c
Fernando Garcia <tqi_fegarcia@uolinc.com>
2023-04-17 23:01:15 -0300

    utilizando o pattern robot no teste de UI

--- app/src/androidTest/java/com/fgarcia/kotlinstars/presentation/list/ListStarsFragmentTest.kt
+++ app/src/androidTest/java/com/fgarcia/kotlinstars/presentation/list/ListStarsFragmentTest.kt
@@ -1,28 +1,16 @@
 package com.fgarcia.kotlinstars.presentation.list
 
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.fgarcia.kotlinstars.frameworks.di.BaseUrlModule
 import com.fgarcia.kotlinstars.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import com.fgarcia.kotlinstars.R
-import com.fgarcia.kotlinstars.extension.asJsonString
-import com.fgarcia.kotlinstars.frameworks.di.BaseUrlModule
-import com.fgarcia.kotlinstars.presentation.list.adapter.viewholder.ItemStarsViewHolder
-import dagger.hilt.android.testing.UninstallModules
-import okhttp3.mockwebserver.MockResponse
-import okhttp3.mockwebserver.MockWebServer
-import org.hamcrest.Matchers.not
-import org.junit.After
 
 @RunWith(AndroidJUnit4::class)
 @UninstallModules(BaseUrlModule::class)
@@ -32,77 +20,40 @@ class ListStarsFragmentTest {
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
-    private lateinit var server: MockWebServer
+    private val robot = ListStartsRobot()
 
     @Before
     fun setUp() {
-        server = MockWebServer().apply { start(PORT) }
+        robot.start()
         launchFragmentInHiltContainer<ListStarsFragment>()
     }
 
     @Test
     fun shouldShowListStars_whenViewIsCreated() {
-        server.enqueue(MockResponse().setBody(MOCK_LIST.asJsonString()))
-        onView(
-            withId(R.id.recycler_stars)
-        ).check(
-            matches(isDisplayed())
-        )
-
-        onView(
-            withId(R.id.include_view_stats_error_state)
-        ).check(
-            matches(not(isDisplayed()))
-        )
+        robot.run {
+            mockListStart()
+            checkShowListStars()
+        }
     }
 
     @Test
     fun shouldShowMoreListStars_whenNewPageViewIsCreated() {
-        with(server) {
-            enqueue(MockResponse().setBody(MOCK_LIST.asJsonString()))
-            enqueue(MockResponse().setBody(MOCK_LIST_PAGINATION.asJsonString()))
+        robot.run {
+            mockPagination()
+            checkShowListStars()
+            checkPaginationTextIsDisplayed()
         }
-
-        // Action
-        onView(
-            withId(R.id.recycler_stars)
-        ).perform(
-            RecyclerViewActions
-                .scrollToPosition<ItemStarsViewHolder>(30)
-        )
-
-        // Assert
-        onView(
-            withId(R.id.recycler_stars)
-        ).check(
-            matches(isDisplayed())
-        )
-        onView(withText("ktorio / ktor")).check(
-            matches(
-                isDisplayed()
-            )
-        )
     }
 
     @Test
     fun shouldShowErrorListStars_whenReturnError() {
-        server.enqueue(MockResponse().setResponseCode(404))
-        onView(
-            withId(R.id.text_initial_loading_error)
-        ).check(
-            matches(isDisplayed())
-        )
+        robot.run {
+            mockError()
+            checkShowError()
+        }
     }
 
     @After
-    fun tearDown() {
-        server.shutdown()
-    }
-
-    private companion object {
-        const val PORT = 8080
-        const val MOCK_LIST = "list_star.json"
-        const val MOCK_LIST_PAGINATION = "list_star_page_2.json"
-    }
+    fun tearDown() = robot.stopServer()
 
 }
